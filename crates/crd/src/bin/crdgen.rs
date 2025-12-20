50c6b543ae1f4651457c6c2de0976cd2686cf3d1
Máté Czékus <mate@picloud.hu>
2025-12-20 18:35:32 +0100

    feat: add helm chart with updated crdgen that uses it
    Signed-off-by: Máté Czékus <mate@picloud.hu>

--- crates/crd/src/bin/crdgen.rs
+++ crates/crd/src/bin/crdgen.rs
@@ -1,7 +1,15 @@
-//! Generates yaml CRD resources from rust code and prints it to stdout
+//! Generates yaml CRD resources from rust code.
+//! By default this will target the helm chart's `crds` directory!
+//! Designed to be used inside of a mise command that sets the `CRDS_DIR` environment variable.
+use std::{fs::File, io::Write, path};
+
 use kube::CustomResourceExt;
 use stickerbomb_crd::Labeler;
 
 fn main() {
-    print!("{}", serde_yaml::to_string(&Labeler::crd()).unwrap());
+    let labeler_schema = serde_yaml::to_string(&Labeler::crd()).unwrap();
+    let labeler_crd_path =
+        path::Path::new(&std::env::var_os("CRDS_DIR").unwrap()).join("labeler-crd.yaml");
+    let mut labeler_file = File::create(labeler_crd_path).unwrap();
+    labeler_file.write_all(labeler_schema.as_bytes()).unwrap();
 }
